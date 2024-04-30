825295cc05c3148c2102e1f75c425b50fdd3e108
KaiwenZheng7609 <helloworld.zheng@mail.utoronto.ca>
2024-04-30 16:58:25 -0400

    updated fetch_query() to allow specified start for each category search

--- parser/parser-engine/parser.c
+++ parser/parser-engine/parser.c
@@ -1,33 +1,237 @@
+#include <sys/stat.h>
 #include "parser.h"
 
 /**
- * Send a query to the ArXiv API to request papers with categories
- * specified by *args*. The first parameter of *args* stores the
- * relative path of the bash script used to send request to the API,
- * and the rest of *args* are the preferred categories collected from
- * the user, except that the last entry of *args* stores NULL that is
- * use to signal the end of *args*.
+ * Assume START and MAX are always preceded by a type of search query
+ * (e.g., cat, au, etc.).
  * **/
-int query(char** args) {
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(1);
-    } else if (pid == 0) {
-        execvp(QUERY_DRIVER, args);
-        perror("execvp");
-        exit(1);
-    } else {
-        int status;
-        if (wait(&status)) {
-            if (WIFEXITED(status) == 1) {
-                exit(1);
-            } else {
-                exit(0);
-            }
-        }
+#define DEFAULT_URL "http://export.arxiv.org/api/query?search_query="
+#define CAT "cat:"
+#define START "&start="
+#define MAX "&max_results="
+#define MAX_RESULTS 10
+
+struct _xml_t {
+    uint8_t* content;
+    size_t nmemb;
+};
+
+FILE* _fopen(const char* path) {
+    FILE* file = fopen(path, "w");
+    if (file == NULL) {
+        perror("fopen");
+        return NULL;
+    }
+    fprintf(file, "%s", "");
+    if (fclose(file) == EOF) {
+        perror("fclose");
+        return NULL;
+    }
+    file = fopen(path, "a");
+    if (file == NULL) {
+        perror("fopen");
+        return NULL;
+    }
+    return file;
+}
+
+size_t _int2str_len(size_t n) {
+    return (n <= 9) ?
+            1 : 1 + _int2str_len(n / 10);
+}
+
+char* _int2str(size_t n) {
+    size_t len = _int2str_len(n);
+    char* strized = malloc(sizeof(char) * (len + 1));
+    if (strized == NULL) {
+        perror("malloc");
+        return NULL;
+    }
+    for (int i = len - 1; i >= 0; i--) {
+        strized[i] = (char) (n % 10 + '0');
+        n /= 10;
+    }
+    return strized;
+}
+
+size_t _write_callback(void* ptr, size_t size, size_t nmemb, void* data) {
+    struct _xml_t* xml = (struct _xml_t *)data;
+    size_t total_size = size * nmemb;
+    size_t new_nmemb = xml->nmemb + total_size;
+    uint8_t old_content[xml->nmemb];
+    memcpy(old_content, xml->content, xml->nmemb);
+    uint8_t* old_ptr = xml->content;
+    xml->content = malloc(sizeof(uint8_t) * new_nmemb);
+    if (xml->content == NULL) {
+        perror("malloc");
+        xml->content = old_ptr;
         exit(1);
     }
+    free(old_ptr), old_ptr = NULL;
+    memcpy(xml->content, old_content, xml->nmemb);
+    memcpy(xml->content + xml->nmemb, ptr, total_size);
+    xml->nmemb = new_nmemb;
+    return total_size;
+}
+
+char* build_query(size_t start, const char* category) {
+    char* start_str = _int2str(start);
+    char* max_str = _int2str(MAX_RESULTS);
+	size_t length = strlen(DEFAULT_URL) + strlen(CAT) + strlen(category) +
+                    strlen(START) + strlen(start_str) + strlen(MAX) + strlen(max_str);
+	char* query = malloc(sizeof(char) * (length + 1));
+	if (query == NULL) {
+		perror("malloc");
+		return NULL;
+	}
+	strncpy(query, DEFAULT_URL, strlen(DEFAULT_URL));
+	query[strlen(DEFAULT_URL)] = '\0';
+	strncat(query, CAT, strlen(CAT));
+	strncat(query, category, strlen(category));
+    strncat(query, START, strlen(START));
+    strncat(query, start_str, strlen(start_str));
+    strncat(query, MAX, strlen(MAX));
+    strncat(query, max_str, strlen(max_str));
+	query[length] = '\0';
+	return query;
+}
+
+int fetch_response(const size_t* starts, const char** args) {
+    /**
+     * Get the number of categories the user has selected for
+     * recommendation.
+     * **/
+    size_t num_cats = 0;
+    for (size_t i = 1; args[i]; i++) { num_cats++; }
+    /**
+     * For each selected category, fork a new process where an API
+     * call is made to fetch all the corresponding papers.
+     * TODO: try to limit the number of papers fetched.
+     * **/
+	int fds[num_cats + 1][2];
+	for (size_t i = 1; i <= num_cats; i++) {
+		if (pipe(fds[i]) < 0) {
+			perror("pipe");
+			return 1;
+		}
+		pid_t pid = fork();
+		if (pid < 0) {
+			perror("fork");
+			exit(1);
+		} else if (pid == 0) {
+			for (size_t j = 1; j <= i; j++) {
+				close(fds[j][0]);
+			}
+			char* request = build_query(starts[i], args[i]);
+            printf("[child] request : %s\n", request);
+			if (request == NULL) {
+				close(fds[i][1]);
+				exit(1);
+			}
+            CURL* curl = curl_easy_init();
+            if (!curl) {
+                fprintf(stderr, "error: failed to initialize CURL object\n");
+                close(fds[i][1]);
+                exit(1);
+            }
+            struct _xml_t data = {NULL, 0};
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, request);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, _write_callback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+            if (res != CURLE_OK) {
+                fprintf(stderr, "error: API request failed\n");
+                close(fds[i][1]);
+                free(request), request = NULL;
+                exit(1);
+            }
+            if (data.nmemb == 0) {
+                close(fds[i][1]);
+                free(request), request = NULL;
+                exit(1);
+            }
+            ssize_t written;
+            written = write(fds[i][1], &data.nmemb, sizeof(size_t));
+            if (written == -1) {
+                perror("write");
+                close(fds[i][1]);
+                free(request), request = NULL;
+                exit(1);
+            }
+            written = write(fds[i][1], data.content, sizeof(uint8_t) * data.nmemb);
+            if (written == -1) {
+                perror("write");
+                close(fds[i][1]);
+                free(request), request = NULL;
+                free(data.content), data.content = NULL;
+                exit(1);
+            }
+            free(data.content), data.content = NULL;
+            free(request), request = NULL;
+            close(fds[i][1]);
+            exit(0);
+		} else {
+			close(fds[i][1]);
+		}
+	}
+    int exit_code = 0;
+    FILE* dest = _fopen(args[0]);
+    if (dest == NULL) {
+        exit_code = 1;
+        goto cleanup;
+    }
+    for (size_t i = 1; i <= num_cats; i++) {
+        int status;
+        if (wait(&status) < 0) {
+            perror("wait");
+            exit_code = 1;
+            goto cleanup;
+        }
+        if (WIFEXITED(status)) {
+            if (WEXITSTATUS(status) == 1) {
+                exit_code = 1;
+                goto cleanup;
+            }
+            size_t nmemb;
+            uint8_t* byte_content;
+            ssize_t num_read;
+            num_read = read(fds[i][0], &nmemb, sizeof(size_t));
+            if (num_read < 0) {
+                perror("read");
+                exit_code = 1;
+                goto cleanup;
+            }
+            byte_content = malloc(sizeof(uint8_t) * nmemb);
+            if (byte_content == NULL) {
+                perror("malloc");
+                exit_code = 1;
+                goto cleanup;
+            }
+            num_read = read(fds[i][0], byte_content, sizeof(uint8_t) * nmemb);
+            if (num_read < 0) {
+                perror("read");
+                close(fds[i][0]);
+                return 1;
+            }
+            char content_buf[nmemb + 1];
+            memcpy(content_buf, (char *)byte_content, nmemb);
+            content_buf[nmemb] = '\0';
+            fprintf(dest, "%s\n", content_buf);
+        }
+    }
+    if (fclose(dest) == EOF) {
+        perror("fclose");
+        return 1;
+    }
+cleanup:
+    if (exit_code == 1) {
+        for (size_t i = 1; i <= num_cats; i++) {
+            close(fds[i][0]);
+        }
+    }
+    return exit_code;
 }
 
 /**
--- parser/parser-engine/parser.h
+++ parser/parser-engine/parser.h
@@ -1,14 +1,18 @@
 #ifndef XML_PARSER_PARSER_H
 #define XML_PARSER_PARSER_H
 
+#include <assert.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <curl/curl.h>
 
-#define QUERY_DRIVER "../parser-util/driver.sh"
+
 #define RESPONSE_LOC "../parser-tmp/response.xml"
-#define NAMESPACE "ns="http://www.w3.org/2005/Atom""
+#define NAMESPACE "ns=\"http://www.w3.org/2005/Atom\""
 
 struct author_t {
     char* name;
@@ -23,7 +27,11 @@ struct paper_t {
     char* jour_ref;
 };
 
-int query(char**);
+
+char* build_query(size_t, const char*);
+
+int fetch_response(const size_t*, const char**);
+
 struct paper_t* parse(const char*);
 
 #endif
